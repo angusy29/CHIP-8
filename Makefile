551e2ec961f09e22419e8c89c378cdc511829d60
Angus Yuen <angus.yuen29@gmail.com>
2018-09-16 17:28:44 +1000

    Added SDL

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 Game: game.o mmu.o chip_8.o graphics.o
-	g++ game.o mmu.o chip_8.o graphics.o -o Game
+	g++ game.o mmu.o chip_8.o graphics.o -o Game -I include -L lib -l SDL2-2.0.0
 
 game.o: game.cpp chip_8.h mmu.h graphics.h
-	g++ -std=c++14 -c game.cpp
+	g++ -std=c++14 -c game.cpp -I include -L lib -l SDL2-2.0.0
 
 mmu.o: mmu.cpp mmu.h
 	g++ -std=c++14 -c mmu.cpp
--- game.cpp
+++ game.cpp
@@ -2,6 +2,7 @@
 #include "chip_8.h"
 #include "mmu.h"
 #include "graphics.h"
+#include <SDL2/SDL.h>
 
 int main() {
     MMU mmu;
@@ -11,12 +12,29 @@ int main() {
     mmu.load_game("pong.rom");
     // mmu.print_rom();
 
-    // for (;;) {
+    int w = 1024;
+    int h = 512;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        std::cout << "SDL could not be initialized, SDL_Error: " << SDL_GetError() << "\n";
+        exit(1);
+    }
+
+    SDL_Window* window = SDL_CreateWindow("CHIP-8 Emulator",
+                            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                            w, h, SDL_WINDOW_SHOWN);
+
+    if (window == NULL) {
+        std::cout << "SDL could not create window, SDL_Error: " << SDL_GetError() << "\n";
+        exit(1);
+    }
+
+    for (;;) {
         // fetch next instruction
         // decode instruction
         // execute
     //     chip.emulate_cycle();
-    // }
+    }
 
     return 0;
 }
