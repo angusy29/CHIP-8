723705915f4da04e4ed2643d73eaf5ba2e4c8d32
Angus Yuen <angus.yuen29@gmail.com>
2018-09-17 21:06:22 +1000

    Split into functions

--- game.cpp
+++ game.cpp
@@ -26,17 +26,59 @@ uint8_t keymap[16] = {
     SDLK_v,
 };
 
-int main() {
+void check_input(CHIP_8 &chip, SDL_Event &e) {
+    if (e.type == SDL_QUIT) exit(0);
+
+    if (e.type == SDL_KEYDOWN) {
+        if (e.key.keysym.sym == SDLK_ESCAPE) exit(0);
+
+        for (auto i = 0; i < 16; ++i) {
+            if (e.key.keysym.sym == keymap[i]) {
+                chip.set_key(i);
+            }
+        }
+    }
+
+    if (e.type == SDL_KEYUP) {
+        for (auto i = 0; i < 16; ++i) {
+            if (e.key.keysym.sym == keymap[i]) {
+                chip.unset_key(i);
+            }
+        }
+    }
+}
+
+void render_screen(CHIP_8 &chip, SDL_Texture *sdlTexture, SDL_Renderer *renderer) {
+    uint32_t pixels[2048];
+
+    for (auto i = 0; i < 2048; ++i) {
+        uint8_t pixel = chip.get_graphics()._display_buffer[i];
+        pixels[i] = (0x00FFFFFF * pixel) | 0xFF000000;
+    }
+
+    // Update SDL texture
+    SDL_UpdateTexture(sdlTexture, NULL, pixels, 64 * sizeof(Uint32));
+    // Clear screen and render
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, sdlTexture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: ./Game <gamefile>\n";
+        std::cout << "Example usage: ./Game pong.rom\n";
+        exit(1);
+    }
+
     MMU mmu;
-    mmu.load_game("pong.rom");
+    mmu.load_game(argv[1]);
 
     Graphics graphics;
     CHIP_8 chip(mmu, graphics);
 
-    chip.get_mmu().print_rom();
-
-    int w = 1024;
-    int h = 512;
+    int width = 1024;
+    int height = 512;
 
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         std::cout << "SDL could not be initialized, SDL_Error: " << SDL_GetError() << "\n";
@@ -45,7 +87,7 @@ int main() {
 
     SDL_Window* window = SDL_CreateWindow("CHIP-8 Emulator",
                             SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                            w, h, SDL_WINDOW_SHOWN);
+                            width, height, SDL_WINDOW_SHOWN);
 
     if (window == NULL) {
         std::cout << "SDL could not create window, SDL_Error: " << SDL_GetError() << "\n";
@@ -53,55 +95,24 @@ int main() {
     }
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-    SDL_RenderSetLogicalSize(renderer, w, h);
+    SDL_RenderSetLogicalSize(renderer, width, height);
 
     SDL_Texture *sdlTexture = SDL_CreateTexture(renderer,
                                 SDL_PIXELFORMAT_ARGB8888,
                                 SDL_TEXTUREACCESS_STREAMING,
                                 64, 32);
 
-    uint32_t pixels[2048];
-
     for (;;) {
         chip.emulate_cycle();
 
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) exit(0);
-
-            if (e.type == SDL_KEYDOWN) {
-                if (e.key.keysym.sym == SDLK_ESCAPE) exit(0);
-
-                for (auto i = 0; i < 16; ++i) {
-                    if (e.key.keysym.sym == keymap[i]) {
-                        chip.set_key(i);
-                    }
-                }
-            }
-
-            if (e.type == SDL_KEYUP) {
-                for (auto i = 0; i < 16; ++i) {
-                    if (e.key.keysym.sym == keymap[i]) {
-                        chip.unset_key(i);
-                    }
-                }
-            }
+            check_input(chip, e);
         }
 
         if (chip.get_draw_flag()) {
             chip.set_draw_flag(false);
-
-            for (auto i = 0; i < 2048; ++i) {
-                uint8_t pixel = chip.get_graphics()._display_buffer[i];
-                pixels[i] = (0x00FFFFFF * pixel) | 0xFF000000;
-            }
-
-            // Update SDL texture
-            SDL_UpdateTexture(sdlTexture, NULL, pixels, 64 * sizeof(Uint32));
-            // Clear screen and render
-            SDL_RenderClear(renderer);
-            SDL_RenderCopy(renderer, sdlTexture, NULL, NULL);
-            SDL_RenderPresent(renderer);
+            render_screen(chip, sdlTexture, renderer);
         }
 
         std::this_thread::sleep_for(std::chrono::microseconds(1200));
